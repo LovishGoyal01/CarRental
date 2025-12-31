3625bd0a630ec28f3e8260985d827907214eb332
LovishGoyal01 <goyallovish852@gmail.com>
2025-12-31 16:49:00 +0530

    links added

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -1,6 +1,8 @@
 import React from 'react'
 import {assets} from '../assets/assets'
 import {motion} from "motion/react"
+import { Link } from "react-router-dom";
+
 
 const Footer = () => {
   return (
@@ -12,10 +14,10 @@ const Footer = () => {
                         Premium car rental service with a wide selection of luxury and everyday vehicles for all your driving needs.
                     </motion.p>
                     <motion.div initial={{opacity:0}} whileInView={{opacity:1}} transition={{duration: 0.5,delay:0.5}} className='flex items-center gap-3 mt-6'>
-                       <a href="#"><img src={assets.facebook_logo} className="w-5 h-5" alt="" /></a>
-                       <a href="#"><img src={assets.instagram_logo} className="w-5 h-5" alt="" /></a>
-                       <a href="#"><img src={assets.twitter_logo} className="w-5 h-5" alt="" /></a>
-                       <a href="#"><img src={assets.gmail_logo} className="w-5 h-5" alt="" /></a>
+                       <a href="https://www.facebook.com/lovish.goyal.9619/" target="_blank" rel="noopener noreferrer"><img src={assets.facebook_logo} className="w-5 h-5" alt="" /></a>
+                       <a href="https://www.instagram.com/lovishgoyal15/" target="_blank" rel="noopener noreferrer"><img src={assets.instagram_logo} className="w-5 h-5" alt="" /></a>
+                       <a href="https://x.com/LovishGoyal2005" target="_blank" rel="noopener noreferrer"><img src={assets.twitter_logo} className="w-5 h-5" alt="" /></a>
+                       <a href="mailto:goyallovish852@gmail.com"><img src={assets.gmail_logo} className="w-5 h-5" alt="" /></a>
                     </motion.div>
                 </div>
 
@@ -23,10 +25,11 @@ const Footer = () => {
                     <div>
                        <h2 className='text-base font-medium text-gray-800 uppercase'>Quick Links</h2>
                        <ul className='mt-3 flex flex-col gap-1.5 '>
-                           <li><a href="#">Home</a></li>
-                           <li><a href="#">Browse Cars</a></li>
-                           <li><a href="#">List Your Car</a></li>
-                           <li><a href="#">About Us</a></li>
+                           <li><Link to="/">Home</Link></li>
+                           <li><Link to="/cars">Browse Cars</Link></li>
+                           <li><Link to="/owner">List Your Car</Link></li>
+                           <li><Link to="/my-bookings">Your Bookings</Link></li>
+
                        </ul>
                     </div>
                     <div>
@@ -43,15 +46,16 @@ const Footer = () => {
                        <ul className='mt-3 flex flex-col gap-1.5 '>
                            <li>1234 Luxury Drive</li>
                            <li>San Francisco, CA 94107</li>
-                           <li>+1 234567890</li>
-                           <li>info@example.com</li>
+                           <li><a href="tel:+917009216273">+91 7009216273</a></li>
+                           <li><a href="mailto:goyallovish852@gmail.com">goyallovish852@gmail.com</a></li>
+
                        </ul>
                     </div> 
                 </motion.div>    
             </motion.div>
 
             <motion.div initial={{y:0, opacity:0}} whileInView={{y:0, opacity:1}} transition={{duration: 0.6,delay:0.6}} className='flex flex-col md:flex-row gap-2 items-center justify-between py-5'>
-                <p>© {new Date().getFullYear()} <a href="https://prebuiltui.com">PrebuiltUI</a>. All rights reserved.</p>
+                <p>© {new Date().getFullYear()} CarRental. All rights reserved.</p>
                 <ul className='flex items-center gap-4'>
                     <li><a href="#">Privacy</a></li>
                     <li>|</li>
